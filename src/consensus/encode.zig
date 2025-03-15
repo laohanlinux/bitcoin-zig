a0aa01ebf5beb4003f740e0219a8d746b20a13a3
zhenghuarong <zhenghuarong@joyy.com>
2025-03-15 19:06:11 +0800

    L\

--- src/consensus/encode.zig
+++ src/consensus/encode.zig
@@ -100,7 +100,7 @@ pub fn Encodable(comptime T: type) type {
         pub fn consensus_encode(self: @This(), writer: anytype) !usize {
             return switch (T) {
                 bool => {
-                    const n = if (self.value) 1 else 0;
+                    const n: u8 = if (self.value) 1 else 0;
                     try writer.writeByte(n);
                     return 1;
                 },
@@ -136,6 +136,10 @@ pub fn Encodable(comptime T: type) type {
                     try writer.writeInt(i64, self.value, .little);
                     return 8;
                 },
+                [2]u8, [4]u8, [8]u8, [12]u8, [16]u8, [32]u8, [33]u8 => {
+                    try writer.writeAll(self.value[0..]);
+                    return self.value.len;
+                },
                 []const u8 => {
                     const varInt = VarInt.init(@as(u64, @intCast(self.value.len)));
                     var len = try Encodable(VarInt).init(varInt).consensus_encode(writer);
@@ -146,24 +150,7 @@ pub fn Encodable(comptime T: type) type {
                     return len;
                 },
                 VarInt => {
-                    switch (self.value.value) {
-                        0...0xFC => {
-                            const n = @as(u8, @intCast(self.value.value));
-                            return Encodable(u8).init(n).consensus_encode(writer);
-                        },
-                        0xFD...0xFFFF => {
-                            try writer.writeByte(0xFD);
-                            return Encodable(u16).init(@intCast(self.value.value)).consensus_encode(writer);
-                        },
-                        0x10000...0xFFFFFFFF => {
-                            try writer.writeByte(0xFE);
-                            return try Encodable(u32).init(@intCast(self.value.value)).consensus_encode(writer);
-                        },
-                        else => {
-                            try writer.writeByte(0xFF);
-                            return Encodable(u64).init(self.value.value).consensus_encode(writer);
-                        },
-                    }
+                    return self.value.consensus_encode(writer);
                 },
                 CheckedData => {
                     const dataLen = @as(u32, @intCast(T.data.len));
@@ -224,31 +211,8 @@ pub fn Decodable(comptime T: type) type {
                     return .{ .value = n, .bytes_read = 8 };
                 },
                 VarInt => {
-                    const n = try reader.readByte();
-                    switch (n) {
-                        0xFF => {
-                            const x = try reader.readInt(u64, .little);
-                            if (x < 0x100000000) {
-                                return Error.NonMinimalVarInt;
-                            }
-                            return .{ .value = VarInt{ .value = x }, .bytes_read = 9 };
-                        },
-                        0xFE => {
-                            const x = try reader.readInt(u32, .little);
-                            if (x < 0x10000) {
-                                return Error.NonMinimalVarInt;
-                            }
-                            return .{ .value = VarInt{ .value = x }, .bytes_read = 5 };
-                        },
-                        0xFD => {
-                            const x = try reader.readInt(u16, .little);
-                            if (x < 0xFD) {
-                                return Error.NonMinimalVarInt;
-                            }
-                            return .{ .value = VarInt{ .value = x }, .bytes_read = 3 };
-                        },
-                        else => return .{ .value = VarInt{ .value = n }, .bytes_read = 1 },
-                    }
+                    const result = try VarInt.consensus_decode(reader);
+                    return .{ .value = result.value, .bytes_read = result.bytes_read };
                 },
                 CheckedData => {
                     @compileError("CheckedData is not supported, use consensus_decode_with_allocator instead");
@@ -260,6 +224,11 @@ pub fn Decodable(comptime T: type) type {
         /// 使用指定的allocator解码T
         pub fn consensus_decode_with_allocator(allocator: std.mem.Allocator, reader: anytype) !struct { value: T, bytes_read: usize } {
             switch (T) {
+                [2]u8, [4]u8, [8]u8, [12]u8, [16]u8, [32]u8, [33]u8 => {
+                    const buffer = try allocator.alloc(u8, T.len);
+                    const n = try reader.readAll(buffer);
+                    return .{ .value = buffer, .bytes_read = n };
+                },
                 CheckedData => {
                     var dataLen: u32 = try reader.readInt(u32, .little);
                     if (dataLen > MAX_VEC_SIZE) {
@@ -361,55 +330,302 @@ pub const VarInt = struct {
             else => return VarInt.init(n),
         };
     }
+
+    /// 编码VarInt, consensus_encode(writer)
+    pub fn consensus_encode(self: VarInt, writer: anytype) !usize {
+        switch (self.value) {
+            0...0xFC => {
+                const n = @as(u8, @intCast(self.value));
+                return Encodable(u8).init(n).consensus_encode(writer);
+            },
+            0xFD...0xFFFF => {
+                try writer.writeByte(0xFD);
+                return Encodable(u16).init(@intCast(self.value)).consensus_encode(writer);
+            },
+            0x10000...0xFFFFFFFF => {
+                try writer.writeByte(0xFE);
+                return try Encodable(u32).init(@intCast(self.value)).consensus_encode(writer);
+            },
+            else => {
+                try writer.writeByte(0xFF);
+                return Encodable(u64).init(self.value).consensus_encode(writer);
+            },
+        }
+    }
+
+    /// 解码VarInt, consensus_decode(reader)
+    pub fn consensus_decode(reader: anytype) !struct { value: VarInt, bytes_read: usize } {
+        const n = try reader.readByte();
+        switch (n) {
+            0xFF => {
+                const x = try reader.readInt(u64, .little);
+                if (x < 0x100000000) {
+                    return Error.NonMinimalVarInt;
+                }
+                return .{ .value = VarInt{ .value = x }, .bytes_read = 9 };
+            },
+            0xFE => {
+                const x = try reader.readInt(u32, .little);
+                if (x < 0x10000) {
+                    return Error.NonMinimalVarInt;
+                }
+                return .{ .value = VarInt{ .value = x }, .bytes_read = 5 };
+            },
+            0xFD => {
+                const x = try reader.readInt(u16, .little);
+                if (x < 0xFD) {
+                    return Error.NonMinimalVarInt;
+                }
+                return .{ .value = VarInt{ .value = x }, .bytes_read = 3 };
+            },
+            else => return .{ .value = VarInt{ .value = n }, .bytes_read = 1 },
+        }
+    }
 };
 
 pub const CheckedData = struct {
     data: []const u8,
     allocator: std.mem.Allocator,
+
+    // 编码CheckedData
+    // 4字节数据长度 + 4字节校验和 + 数据
+    pub fn consensus_encode(self: *const CheckedData, writer: anytype) !usize {
+        try Encodable(u32).init(@as(u32, @intCast(self.data.len))).consensus_encode(writer);
+        const checkSum = sha2CheckSum(self.data);
+        try Encodable([4]u8).init(checkSum).consensus_encode(writer);
+        try writer.writeAll(self.data);
+        return 8 + self.data.len;
+    }
+
+    /// 使用指定的allocator解码CheckedData
+    /// 返回值：CheckedData
+    pub fn consensus_decode_with_allocator(allocator: std.mem.Allocator, reader: anytype) !CheckedData {
+        var dataLen = try Decodable(u32).consensus_decode(reader);
+        if (dataLen.value > MAX_VEC_SIZE) {
+            return Error.OversizedVectorAllocation;
+        }
+
+        const checkSumLen = try Decodable([4]u8).consensus_decode_with_allocator(allocator, reader);
+        defer allocator.free(checkSumLen.value);
+        std.testing.expect(checkSumLen.bytes_read == 4);
+
+        const data = try allocator.alloc(u8, dataLen.value);
+        dataLen = try reader.read(data);
+        std.testing.expect(dataLen == data.len);
+
+        const checkSum = sha2CheckSum(data);
+        std.testing.expect(mem.eql(u8, checkSum, checkSumLen.value));
+        return .{ .data = data, .allocator = allocator };
+    }
 };
 
-// /// 对数据进行双SHA256并返回前4个字节
-// fn sha2_checksum(data: []const u8) [4]u8 {
-//     var sha256d = Sha256D.init(.{});
-//     sha256d.update(data);
-//     var checksum: [32]u8 = undefined;
-//     sha256d.finish(&checksum);
-//     return [4]u8{ checksum[0], checksum[1], checksum[2], checksum[3] };
-// }
+/// 编码数据并返回长度
+pub fn consensusEncodeWithSize(data: []const u8, writer: anytype) !usize {
+    const vi_len = VarInt.init(@as(u64, @intCast(data.len))).len();
+    try writer.writeInt(u32, data.len, .little);
+    try writer.writeAll(data);
+    return vi_len + data.len;
+}
 
-// /// 将对象编码到字节数组
-// pub fn serialize(allocator: std.mem.Allocator, data: anytype) ![]u8 {
-//     var list = std.ArrayList(u8).init(allocator);
-//     defer list.deinit();
+fn testVarIntEncode(n: u8, x: []const u8) !VarInt {
+    var input = [_]u8{0} ** 9;
+    input[0] = n;
+    @memcpy(input[1..], x);
+    const value = try deserializePartial(VarInt, &input);
+    return value.value;
+}
 
-//     const writer = list.writer();
-//     try encode(data, writer);
-
-//     return list.toOwnedSlice();
-// }
-
-// /// 将对象编码为十六进制字符串
-// pub fn serialize_hex(allocator: std.mem.Allocator, data: anytype) ![]u8 {
-//     const bytes = try serialize(allocator, data);
-//     defer allocator.free(bytes);
-
-//     return hex.encode(allocator, bytes);
-// }
-
-// /// 从字节数组反序列化对象，如果未消耗整个数组则报错
-// pub fn deserialize(comptime T: type, allocator: Allocator, data: []const u8) !T {
-//     var stream = io.fixedBufferStream(data);
-//     const reader = stream.reader();
-
-//     const result = try decode(T, allocator, reader);
-
-//     // 确保数据被完全消耗
-//     if (stream.pos != data.len) {
-//         return Error.ParseFailed;
+//   fn test_varint_len(varint: VarInt, expected: usize) {
+//         let mut encoder = io::Cursor::new(vec![]);
+//         assert_eq!(varint.consensus_encode(&mut encoder).unwrap(), expected);
+//         assert_eq!(varint.len(), expected);
 //     }
 
-//     return result;
-// }
+fn testVarIntLen(varint: VarInt, expected: usize) void {
+    const buffer = std.testing.allocator.alloc(u8, expected) catch unreachable;
+    defer std.testing.allocator.free(buffer);
+    const encoder = io.fixedBufferStream(buffer).writer();
+    const len = varint.consensus_encode(encoder) catch unreachable;
+    std.testing.expect(len == expected);
+}
+
+fn u64ToArrayLe(n: u64) [8]u8 {
+    var buffer = [8]u8{ 0, 0, 0, 0, 0, 0, 0, 0 };
+    var fbs = std.io.fixedBufferStream(&buffer);
+    fbs.writer().writeInt(u64, n, .little) catch unreachable;
+    return buffer;
+}
+
+test "serialize_int_test" {
+    // bool
+    {
+        const args = [_]bool{ false, true };
+        const expected = [_][]const u8{
+            &[_]u8{0},
+            &[_]u8{1},
+        };
+        for (args, expected) |arg, exp| {
+            const allocator = testing.allocator;
+            const encoded = try serialize(allocator, arg);
+            defer allocator.free(encoded);
+            try testing.expect(std.mem.eql(u8, encoded, exp));
+        }
+    }
+
+    // u8
+    {
+        const args = [_]u8{ 1, 0, 255 };
+        const expected = [_][]const u8{
+            &[_]u8{1},
+            &[_]u8{0},
+            &[_]u8{255},
+        };
+        for (args, expected) |arg, exp| {
+            const allocator = testing.allocator;
+            const encoded = try serialize(allocator, arg);
+            defer allocator.free(encoded);
+            try testing.expect(std.mem.eql(u8, encoded, exp));
+        }
+    }
+
+    // u16
+    {
+        const args = [_]u16{ 1, 256, 5000 };
+        const expected = [_][]const u8{
+            &[_]u8{ 1, 0 },
+            &[_]u8{ 0, 1 },
+            &[_]u8{ 136, 19 },
+        };
+        for (args, expected) |arg, exp| {
+            const encoded = try serialize(testing.allocator, arg);
+            defer testing.allocator.free(encoded);
+            try testing.expect(std.mem.eql(u8, encoded, exp));
+        }
+    }
+
+    // u32
+    {
+        const args = [_]u32{ 1, 256, 5000, 500000, 168430090 };
+        const expected = [_][]const u8{
+            &[_]u8{ 1, 0, 0, 0 },
+            &[_]u8{ 0, 1, 0, 0 },
+            &[_]u8{ 136, 19, 0, 0 },
+            &[_]u8{ 32, 161, 7, 0 },
+            &[_]u8{ 10, 10, 10, 10 },
+        };
+        for (args, expected) |arg, exp| {
+            const encoded = try serialize(testing.allocator, arg);
+            defer testing.allocator.free(encoded);
+            try testing.expect(std.mem.eql(u8, encoded, exp));
+        }
+    }
+
+    // i32
+    {
+        const args = [_]i32{ -1, -256, -5000, -500000, -168430090, 1, 256, 5000, 500000, 168430090 };
+        const expected = [_][]const u8{
+            &[_]u8{ 255, 255, 255, 255 },
+            &[_]u8{ 0, 255, 255, 255 },
+            &[_]u8{ 120, 236, 255, 255 },
+            &[_]u8{ 224, 94, 248, 255 },
+            &[_]u8{ 246, 245, 245, 245 },
+            &[_]u8{ 1, 0, 0, 0 },
+            &[_]u8{ 0, 1, 0, 0 },
+            &[_]u8{ 136, 19, 0, 0 },
+            &[_]u8{ 32, 161, 7, 0 },
+            &[_]u8{ 10, 10, 10, 10 },
+        };
+        for (args, expected) |arg, exp| {
+            const encoded = try serialize(testing.allocator, arg);
+            defer testing.allocator.free(encoded);
+            try testing.expect(std.mem.eql(u8, encoded, exp));
+        }
+    }
+
+    // u64
+    {
+        const args = [_]u64{ 1, 256, 5000, 500000, 723401728380766730 };
+        const expected = [_][]const u8{
+            &[_]u8{ 1, 0, 0, 0, 0, 0, 0, 0 },
+            &[_]u8{ 0, 1, 0, 0, 0, 0, 0, 0 },
+            &[_]u8{ 136, 19, 0, 0, 0, 0, 0, 0 },
+            &[_]u8{ 32, 161, 7, 0, 0, 0, 0, 0 },
+            &[_]u8{ 10, 10, 10, 10, 10, 10, 10, 10 },
+        };
+        for (args, expected) |arg, exp| {
+            const encoded = try serialize(testing.allocator, arg);
+            defer testing.allocator.free(encoded);
+            try testing.expect(std.mem.eql(u8, encoded, exp));
+        }
+    }
+
+    // i64
+    {
+        const args = [_]i64{ -1, -256, -5000, -500000, -723401728380766730, 1, 256, 5000, 500000, 723401728380766730 };
+        const expected = [_][]const u8{
+            &[_]u8{ 255, 255, 255, 255, 255, 255, 255, 255 },
+            &[_]u8{ 0, 255, 255, 255, 255, 255, 255, 255 },
+            &[_]u8{ 120, 236, 255, 255, 255, 255, 255, 255 },
+            &[_]u8{ 224, 94, 248, 255, 255, 255, 255, 255 },
+            &[_]u8{ 246, 245, 245, 245, 245, 245, 245, 245 },
+            &[_]u8{ 1, 0, 0, 0, 0, 0, 0, 0 },
+            &[_]u8{ 0, 1, 0, 0, 0, 0, 0, 0 },
+            &[_]u8{ 136, 19, 0, 0, 0, 0, 0, 0 },
+            &[_]u8{ 32, 161, 7, 0, 0, 0, 0, 0 },
+            &[_]u8{ 10, 10, 10, 10, 10, 10, 10, 10 },
+        };
+        for (args, expected) |arg, exp| {
+            const encoded = try serialize(testing.allocator, arg);
+            defer testing.allocator.free(encoded);
+            try testing.expect(std.mem.eql(u8, encoded, exp));
+        }
+    }
+}
+
+test "serialize_varint_test" {
+    {
+        const args = [_]VarInt{ VarInt.init(10), VarInt.init(0xFC), VarInt.init(0xFD), VarInt.init(0xFFF), VarInt.init(0xF0F0F0F), VarInt.init(0xF0F0F0F0F0E0) };
+        const encoded = [_][]const u8{
+            &[_]u8{10},
+            &[_]u8{0xFC},
+            &[_]u8{ 0xFD, 0xFD, 0 },
+            &[_]u8{ 0xFD, 0xFF, 0xF },
+            &[_]u8{ 0xFE, 0xF, 0xF, 0xF, 0xF },
+            &[_]u8{ 0xFF, 0xE0, 0xF0, 0xF0, 0xF0, 0xF0, 0xF0, 0, 0 },
+        };
+        for (args, encoded) |arg, exp| {
+            const got = try serialize(testing.allocator, arg);
+            defer testing.allocator.free(got);
+            try testing.expect(std.mem.eql(u8, got, exp));
+        }
+    }
+    {
+        const preArgs = [_]u8{ 0xFF, 0xFE, 0xFD };
+        const args = [_]VarInt{ VarInt.init(0x100000000), VarInt.init(0x10000), VarInt.init(0xFD) };
+        const encoded = [_][8]u8{
+            u64ToArrayLe(0x100000000),
+            u64ToArrayLe(0x10000),
+            u64ToArrayLe(0xFD),
+        };
+        for (0..args.len) |i| {
+            const got = try testVarIntEncode(preArgs[i], &encoded[i]);
+            try testing.expectEqual(got.value, args[i].value);
+        }
+    }
+
+    {
+        // Test that length calc is working correctly
+        // test_varint_len(VarInt(0), 1);
+        // test_varint_len(VarInt(0xFC), 1);
+        // test_varint_len(VarInt(0xFD), 3);
+        // test_varint_len(VarInt(0xFFFF), 3);
+        // test_varint_len(VarInt(0x10000), 5);
+        // test_varint_len(VarInt(0xFFFFFFFF), 5);
+        // test_varint_len(VarInt(0xFFFFFFFF+1), 9);
+        // test_varint_len(VarInt(u64::max_value()), 9);
+
+    }
+}
 
 test "encodable" {
     var _i8 = Encodable(i8).init(1);
